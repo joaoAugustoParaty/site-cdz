79f74ffa87457e1e8f06044b14a4a3a0f956d26b
João Augusto de Oliveira <joaoaugustowebdev@gmail.com>
2021-03-26 19:01:49 -0300

    Terminado

--- script.js
+++ script.js
@@ -1,4 +1,118 @@
 
 function imgSlider(anything){
     document.querySelector('.seiya').src = anything
+    
+}
+
+//Seiya
+
+function changeTextSeiya(){
+    document.getElementById('cavaleiro').innerHTML = 'Cavaleiro de Bronze';
+}
+
+function changeTextnomeSeiya(){
+    document.getElementById('nome').innerHTML = 'Seiya de Pegasus';
+}
+
+function changeTextColorSeiya(){
+    document.getElementById('nome').style.color="#bd0800"
+}
+
+function changeTexttextSeiya(){
+    document.getElementById('texto').innerHTML = 'Cavaleiro de Bronze da constelação de Pégaso do século XX, incumbido com a missão de proteger a deusa Atena nas Guerras Santas no século XX. Caracterizado principalmente por nunca desistir de lutar em qualquer situação, por mais impossível ou difícil que possa parecer, ele também é a reencarnação do Cavaleiro de Bronze Tenma de Pégaso e do Primeiro Pégaso, o primeiro homem a ferir o Deus Hades nos tempos mitológicos.';
+}
+
+function changeTextColorSeiyatext(){
+    document.getElementById('texto').style.color="#fee801"
+}
+
+
+//Shiryu
+
+function changeTextShiryu(){
+    document.getElementById('cavaleiro').innerHTML = 'Cavaleiro de Bronze';
+}
+
+function changeTextnomeShiryu(){
+    document.getElementById('nome').innerHTML = 'Shiryu de Dragão';
+}
+
+function changeTextColorShiryu(){
+    document.getElementById('nome').style.color="#305c64"
+}
+
+function changeTexttextShiryu(){
+    document.getElementById('texto').innerHTML = 'Shiryu de Dragão é um Cavaleiro de Bronze do século XX, incumbido com a missão de proteger a deusa Atena. Teve seu treinamento realizado pelo Mestre Ancião nos Cinco Picos Antigos de Rozan, na China.';
+}
+
+function changeTextColorShiryutext(){
+    document.getElementById('texto').style.color="#72c1ca"
+}
+
+//Shun
+
+function changeTextShun(){
+    document.getElementById('cavaleiro').innerHTML = 'Cavaleiro de Bronze';
+    
+}
+
+function changeTextnomeShun(){
+    document.getElementById('nome').innerHTML = 'Shun de Andrômeda';
+}
+
+function changeTextColorShun(){
+    document.getElementById('nome').style.color="#b96fc6"
+}
+
+function changeTexttextShun(){
+    document.getElementById('texto').innerHTML = 'Shun de Andrômeda é um Cavaleiro de Bronze do século XX, incumbido com a missão de proteger a deusa Atena do mal.';
+}
+
+function changeTextColorShuntext(){
+    document.getElementById('texto').style.color="#ca2b5e"
+}
+
+
+//Hyoga
+
+function changeTextHyoga(){
+    document.getElementById('cavaleiro').innerHTML = 'Cavaleiro de Bronze';
+}
+
+function changeTextnomeHyoga(){
+    document.getElementById('nome').innerHTML = 'Hyoga de Cisne';
+}
+
+function changeTextColorHyoga(){
+    document.getElementById('nome').style.color="#f1fff2"
+}
+
+function changeTexttextHyoga(){
+    document.getElementById('texto').innerHTML = 'Hyoga de Cisne é um Cavaleiro de Bronze do século XX. Um dos 100 órfãos reunidos por Mitsumasa Kido para proteger a deusa Atena. Nas frias terras das Geleiras Eternas, no leste da Sibéria, Hyoga torna-se Cavaleiro tendo como mestre Camus, o Cavaleiro de Ouro de Aquário, que manipula o frio. Seguindo os ensinamentos do seu mestre, ele tenta manter-se racional diante de tudo, mas possui um lado emotivo que não consegue abandonar.';
+}
+
+function changeTextColorHyogatext(){
+    document.getElementById('texto').style.color="#8b8ed7"
+}
+
+//Ikki de Fênix
+
+function changeTextIkki(){
+    document.getElementById('cavaleiro').innerHTML = 'Cavaleiro de Bronze';
+}
+
+function changeTextnomeIkki(){
+    document.getElementById('nome').innerHTML = 'Ikki de Fênix';
+}
+
+function changeTextColorIkki(){
+    document.getElementById('nome').style.color="#ffac55"
+}
+
+function changeTexttextIkki(){
+    document.getElementById('texto').innerHTML = 'Ikki de Fênix é um Cavaleiro de Bronze do século XX e irmão mais velho do também cavaleiro de bronze, Shun de Andrômeda. Na Ilha da Rainha da Morte, onde foi treinado, acaba vendo Esmeralda, seu grande amor, morta por uma falha sua e fica sabendo o terrível segredo de seu nascimento. Tais fatos são suficientes para ele começar a odiar a tudo e a todos. Ikki comandou os Cavaleiros Negros em uma vingança contra Saori Kido, mas se arrependeu e agora luta pela Deusa Atena, ajudando seus amigos e seu irmão sempre que necessário.';
+}
+
+function changeTextColorIkkitext(){
+    document.getElementById('texto').style.color="#5b419d"
 }
