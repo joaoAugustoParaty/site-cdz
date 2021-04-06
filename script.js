d21cd5bfc4330a6e1784cca6b9a6e500ad2a6d7b
João Augusto de Oliveira <joaoaugustowebdev@gmail.com>
2021-04-06 18:01:40 -0300

    Mobile

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function changeTextColorSeiya(){
 }
 
 function changeTexttextSeiya(){
-    document.getElementById('texto').innerHTML = 'Cavaleiro de Bronze da constelação de Pégaso do século XX, incumbido com a missão de proteger a deusa Atena nas Guerras Santas no século XX. Caracterizado principalmente por nunca desistir de lutar em qualquer situação, por mais impossível ou difícil que possa parecer, ele também é a reencarnação do Cavaleiro de Bronze Tenma de Pégaso e do Primeiro Pégaso, o primeiro homem a ferir o Deus Hades nos tempos mitológicos.';
+    document.getElementById('texto').innerHTML = 'Cavaleiro de Bronze da constelação de Pégaso do século XX, incumbido com a missão de proteger a deusa Atena nas Guerras Santas no século XX.';
 }
 
 function changeTextColorSeiyatext(){
@@ -42,7 +42,7 @@ function changeTextColorShiryu(){
 }
 
 function changeTexttextShiryu(){
-    document.getElementById('texto').innerHTML = 'Shiryu de Dragão é um Cavaleiro de Bronze do século XX, incumbido com a missão de proteger a deusa Atena. Teve seu treinamento realizado pelo Mestre Ancião nos Cinco Picos Antigos de Rozan, na China.';
+    document.getElementById('texto').innerHTML = 'Shiryu de Dragão é um Cavaleiro de Bronze do século XX, incumbido com a missão de proteger a deusa Atena.';
 }
 
 function changeTextColorShiryutext(){
@@ -88,7 +88,7 @@ function changeTextColorHyoga(){
 }
 
 function changeTexttextHyoga(){
-    document.getElementById('texto').innerHTML = 'Hyoga de Cisne é um Cavaleiro de Bronze do século XX. Um dos 100 órfãos reunidos por Mitsumasa Kido para proteger a deusa Atena. Nas frias terras das Geleiras Eternas, no leste da Sibéria, Hyoga torna-se Cavaleiro tendo como mestre Camus, o Cavaleiro de Ouro de Aquário, que manipula o frio. Seguindo os ensinamentos do seu mestre, ele tenta manter-se racional diante de tudo, mas possui um lado emotivo que não consegue abandonar.';
+    document.getElementById('texto').innerHTML = 'Hyoga de Cisne é um Cavaleiro de Bronze do século XX. Um dos 100 órfãos reunidos por Mitsumasa Kido para proteger a deusa Atena.';
 }
 
 function changeTextColorHyogatext(){
@@ -110,7 +110,7 @@ function changeTextColorIkki(){
 }
 
 function changeTexttextIkki(){
-    document.getElementById('texto').innerHTML = 'Ikki de Fênix é um Cavaleiro de Bronze do século XX e irmão mais velho do também cavaleiro de bronze, Shun de Andrômeda. Na Ilha da Rainha da Morte, onde foi treinado, acaba vendo Esmeralda, seu grande amor, morta por uma falha sua e fica sabendo o terrível segredo de seu nascimento. Tais fatos são suficientes para ele começar a odiar a tudo e a todos. Ikki comandou os Cavaleiros Negros em uma vingança contra Saori Kido, mas se arrependeu e agora luta pela Deusa Atena, ajudando seus amigos e seu irmão sempre que necessário.';
+    document.getElementById('texto').innerHTML = 'Ikki de Fênix é um Cavaleiro de Bronze do século XX e irmão mais velho do também cavaleiro de bronze, Shun de Andrômeda.';
 }
 
 function changeTextColorIkkitext(){
