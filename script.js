f50739084ee24c46db2dabc99cf80d725a79172c
João Augusto de Oliveira <joaoaugustowebdev@gmail.com>
2021-04-07 10:22:35 -0300

    Site pronto

--- script.js
+++ script.js
@@ -116,3 +116,17 @@ function changeTexttextIkki(){
 function changeTextColorIkkitext(){
     document.getElementById('texto').style.color="#5b419d"
 }
+
+/*_____________MENU____________*/
+
+function toggleMenu() {
+
+	var menu = document.getElementById("menu");
+
+	if (menu.style.display == 'none' || menu.style.display == '') {
+		menu.style.display = "block";
+	} else {
+		menu.style.display = "none";
+	}
+
+}
